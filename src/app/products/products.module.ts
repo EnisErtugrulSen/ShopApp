5aa6b341ac431dfd22a2b14845a999e4dbbf7234
“sadikturan” <sadikturan@gmail.com>
2022-11-08 09:44:03 +0300

    Ders 56-shared-module

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from "@angular/router";
 import { ProductCreateComponent } from "./product-create/product-create.component";
 import { ProductListComponent } from "./product-list/product-list.component";
 import { ProductComponent } from "./product/product.component";
+import { CKEditorModule } from 'ckeditor4-angular';
 
 @NgModule({
     declarations: [
@@ -16,6 +17,7 @@ import { ProductComponent } from "./product/product.component";
         CommonModule,
         RouterModule,
         FormsModule,
+        CKEditorModule
     ],
     exports: [
         ProductListComponent,
